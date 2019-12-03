65695d33b045b3c7562265aec009d4b7b8d7c1e3
majid <majidlotfinia12@gmail.com>
2019-12-03 18:03:52 +0330

    add create card controller

--- api/controllers/CardController.js
+++ api/controllers/CardController.js
@@ -6,7 +6,23 @@
  */
 
 module.exports = {
-  
-
+    create: function (req, res) {
+        console.log('card controller', req.body);
+        req.body.owner = req.user.id;
+        Board.findOne({ id: req.body.belong }, (err, board) => {
+            console.log('error is', err)
+            if (err) {
+                return res.status(400).send({ err: 'an error occured' });
+            } else if (board.owner !== req.user.id) {
+                return res.status(401).send({ err: 'you are not the owner of this board' });
+            }
+            Card.create(req.body, (err, card) => {
+                if (err) {
+                    return res.status(400).send(({ err: 'create card an error occured' }))
+                }
+                return res.status(200).send(card)
+            })
+        });
+    }
 };
 
